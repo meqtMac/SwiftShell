76a48ede994d63fc81a5190b7ddc9752ac712f6a
meqt <meqt@foxmail.com>
2023-06-11 13:22:03 +0800

    Initial Commit

---
+++ Sources/SwiftShell/SwiftShell.swift
@@ -0,0 +1,6 @@
+public struct SwiftShell {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/SwiftShellTests/SwiftShellTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SwiftShell
+
+final class SwiftShellTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SwiftShell().text, "Hello, World!")
+    }
+}
