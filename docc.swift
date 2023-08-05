f637fe517c6c77eff0c44e89d01e90757556c3c3
蒋艺 <meqt@foxmail.com>
2023-08-05 23:40:07 +0800

    save

---
+++ docc.swift
@@ -0,0 +1,23 @@
+//
+//  File.swift
+//  SwiftShell
+//
+//  Created by 蒋艺 on 2023/6/11.
+//
+
+import Foundation
+
+//let article = @Document {
+//    @Column {
+//        @Code{
+//            for x in elements {
+//                y = tan(x) / 2
+//            }
+//        }
+//        
+//        @Code {
+//            y = elements.map{ tan($) / 2 }
+//        }
+//    }
+//}
+//
